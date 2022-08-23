7498978da1635db35c63fcaf0969c42fead00adf
guilhermepracheti <111798656+guilhermepracheti@users.noreply.github.com>
2022-08-22 21:08:28 -0300

    Create calculadora

---
+++ Calculadora.py
@@ -0,0 +1,7 @@
+print("calculadora da Segunda lei de Newton")
+
+ massa = int(input("qual é a massa do corpo?"))
+ 
+ aceleracao = int(input("qual é a aceleração do corpo?"))
+ 
+print("A força em Newtons é equivalente a", massa * aceleracao, "Newtons")
