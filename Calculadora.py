6a682da18076e92d8a6fcee4a748fb3a1a485dcb
guilhermepracheti <111798656+guilhermepracheti@users.noreply.github.com>
2022-08-22 21:23:04 -0300

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,16 @@
+while True:
+ print("qual calculadora vamos usar?")
+ calculadora = int(input("[1] física [2] matemática"))
+ if calculadora ==1:
+ print("***************************************")
+ print("*calculadora da segunda lei de newton *[2]nenhuma \n"))
+ 
+ print("*************************************/n")
+ massa = int(input("qual é a massa do corpo  "))
+ aceleracao = int(input("nqual é a aceleração do corpo"))
+ print("/nA força é equivalente a" , massa *aceleracao,"Newtons/n/n")
+	continua = int(input("vamos continuar?/n[1]sim [2]não"))
+	if continua ==1
+	   continue
+	if continua ==2
+	    break
